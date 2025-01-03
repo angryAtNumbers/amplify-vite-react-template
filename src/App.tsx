4be7909845910414c500203d287a739ebbdaace5
angryAtNumbers <168223460+angryAtNumbers@users.noreply.github.com>
2025-01-02 21:15:11 -0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,26 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
 
-const client = generateClient<Schema>();
+import React from "react";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <div className="app">
+      <header className="header">
+        <div className="logo">Pipit</div>
+        <nav className="nav">
+          <button className="nav-button">Button 1</button>
+          <button className="nav-button">Button 2</button>
+          <button className="nav-button">Button 3</button>
+          <button className="nav-button">Button 4</button>
+        </nav>
+      </header>
+      <main className="main">
+        <h1 className="title">Welcome to the Pipit App</h1>
+        <p className="description">
+          Explore the app and see the minimalist design in action.
+        </p>
+      </main>
+    </div>
   );
 }
 
