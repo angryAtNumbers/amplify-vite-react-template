0d16c2b7abb053948508d268fa080d921988566b
angryAtNumbers <168223460+angryAtNumbers@users.noreply.github.com>
2025-01-02 21:45:28 -0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,6 @@
-
-import React, { useEffect, useState } from "react";
 import "./App.css";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    const content = window.prompt("Todo content");
-    if (content) {
-      client.models.Todo.create({ content });
-    }
-  }
-
   return (
     <div className="app">
       <header className="header">
@@ -34,13 +13,10 @@ function App() {
         </nav>
       </header>
       <main className="main">
-        <h1 className="title">My todos</h1>
-        <button onClick={createTodo} className="create-todo-button">+ new</button>
-        <ul className="todo-list">
-          {todos.map((todo) => (
-            <li key={todo.id} className="todo-item">{todo.content}</li>
-          ))}
-        </ul>
+        <h1 className="title">Welcome to the Pipit App</h1>
+        <p className="description">
+          Explore the app and see the minimalist design in action.
+        </p>
       </main>
     </div>
   );
